4370e177c4121dd785e4ec431f607b43d7178f0c
enmanuelpinate <enmanuelpinate@gmail.com>
2019-09-30 11:27:37 +0200

    class 4

--- @classandstatisticmethod.py
+++ @classandstatisticmethod.py
@@ -1,2 +1,20 @@
 #ClassMethod and StatisticMethod
 
+class Book:
+    TYPES = ("hardcover", "paperback")
+
+    def __init__(self, name, book_type, weight):
+        self.name = name
+        self.book_type = book_type
+        self.weight = weight
+
+    def __repr__(self):
+        return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
+
+    @classmethod
+    def hardcover(cls, name, page_weight):
+        return cls(name, cls.TYPES[0], page_weight + 100)
+
+book = Book.hardcover("Harry Potter", 1500)
+
+print(book)
